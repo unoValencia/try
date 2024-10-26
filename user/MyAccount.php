e81e3db50190267ca2f9c2fd9313f8a28aac8381
unoValencia <valenciajg@students.nu-lipa.edu.ph>
2024-10-26 21:30:02 +0800

    lesson 12

--- user/MyAccount.php
+++ user/MyAccount.php
@@ -1,14 +1,111 @@
 <?php
-
 session_start();
 
 if(isset($_SESSION["email"])){
     $email = $_SESSION["email"];
 }else{
-    echo "<script>window.location/href='../';</script>";
+    echo "<script>window.location.href='../';</script>";
 }
 
-include("nav.php");
+
 include("../connections.php");
 
+    $query_info = mysqli_query($connections, "SELECT * FROM tbl_user WHERE email='$email'");
+    $my_info = mysqli_fetch_assoc($query_info);
+    $account_type = $my_info["account_type"];
+    $img = $my_info["img"];
+    
+include("nav.php");
 ?>
+
+<script src="../Admin/js/jQuery.js"></script>
+
+<style>
+    img{height:150px;}
+</style>
+
+<script>
+    var _URL = window.URL || window.webkitURL;
+
+    function displayPreview(files){
+        var file = files[0];
+        var img = new Image();
+        var sizeKB = file.size / 1024;
+        img.onload = function(){
+            $('#preview').html(img); 
+        }
+        img.src = _URL.createObjectURL(file);
+    }
+</script>
+
+<br>
+<br>
+
+<?php
+$target_dir = "photo_folder/";
+$uploadErr = "";
+
+if(isset($_POST["btnUpload"])){
+    $target_file = $target_dir . "/" . basename($_FILES["profile_pic"]["name"]);
+    $uploadOk = 1;
+
+    if(file_exists($target_file)){
+        $target_file = $target_dir . rand(1,9) . rand(1,9) . rand(1,9) . rand(1,9) . "_" . basename($_FILES["profile_pic"]["name"]);
+        $uploadOk = 1;
+    }
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
+    if($_FILES["profile_pic"]["size"] > 5000000000){
+        $uploadErr = "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif"){
+        $uploadErr = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    if($uploadOk == 1){
+        if(move_uploaded_file($_FILES["profile_pic"]["tmp_name"],$target_file)){
+            mysqli_query($connections, "UPDATE tbl_user SET img='$target_file' WHERE email='$email'");
+            $notify = "<font color=green>Your profile photo has been uploaded! </font>";
+            echo "<script>window.location.href='MyAccount?notify=$notify';</script>";
+        }else{
+            echo "Sorry, there was an error uploadimh your file.";
+        }
+    }
+}
+if(empty($_GET["notify"])){
+
+    // do nothing
+}else{
+  echo "<center>" . $_GET["notify"] . "</center>";
+}
+
+if($img == ""){
+    echo "<center>No Photo</center>";
+} else {
+    echo "<center><img src='$img' height='150px' width='200px'></center>";
+}
+
+?>
+
+<form method="POST" enctype="multipart/form-data">
+    <table border="0" width="30%">
+        <tr><td colspan="2"><center><span id="preview"></span></center></td></tr>
+        <tr><td colspan="2"><hr></td></tr>
+        <tr>
+            <td width="50%"><input type="file" id="profile_pic" name="profile_pic" onchange="displayPreview(this.files)"></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <center>
+                    <input type="submit" name="btnUpload" class="btn-update" value="Upload">
+                </center>
+            </td>
+        </tr>
+    </table>
+</form>
+
+<span class="error"><?php echo $uploadErr; ?></span>
--- user/MyFavorites.php
+++ user/MyFavorites.php
@@ -10,4 +10,106 @@ if(isset($_SESSION["email"])){
 include("../connections.php");
 include("nav.php");
 
+$check = $checkErr = "";
+
+if(isset($_POST["btnSubmit"])){
+    if(empty($_POST["check"])){
+        $checkErr = "Select at least one (1)";
+    }else{
+        $check = $_POST["check"];
+    }
+
+
+    if($check){
+
+        echo "<br><br>";
+
+        foreach($check as $new_check){
+            echo $new_check . "<br>";
+        }
+
+    }
+}
+
 ?>
+
+<hr>
+
+<form method="POST">
+
+    <span class="error"><?php echo $checkErr; ?></span>
+    <input type="checkbox" name="check[]" value="Beer">Beer<br>
+    <input type="checkbox" name="check[]" value="San Miglight Apple">San Miglight Apple<br>
+    <input type="checkbox" name="check[]" value="Alfonso Light">Alfonso Light<br>
+    <input type="checkbox" name="check[]" value="Great Taste White Choco">Great Taste White Choco<br>
+    <input type="checkbox" name="check[]" value="Mojito">Mojito<br>
+    <input type="checkbox" name="check[]" value="Piña Colada">Piña Colada<br>
+    <input type="checkbox" name="check[]" value="Lemon Iced Tea">Lemon Iced Tea<br>
+    <input type="checkbox" name="check[]" value="Matcha Latte">Matcha Latte<br>
+    <input type="checkbox" name="check[]" value="Margarita">Margarita<br>
+    <input type="checkbox" name="check[]" value="Water">Water<br>
+    <input type="submit" name="btnSubmit" value="Submit">
+
+</form>
+
+<hr>
+
+<script type="text/javascript">
+
+var Category = {
+
+    "Car" : ["Honda","BMW","Mustang"],
+    "Food" : ["Burger","Maling","Hotdog"],
+    "Beer" : ["Red Horse","Colt 45","San Mig Light Apple"],
+    "Fruits" : ["Mango","Pineapple","Blueberry"],
+    "Desserts" : ["Cheesecake","Chocolate Mousse","Apple Pie"],
+    "Sports" : ["Soccer","Tennis","Swimming"],
+    "Genres of Music" : ["Jazz","Hip-Hop","Classical"],
+    "Languages" : ["French","Japanese","German"],
+    "Flowers" : ["Rose","Lily","Tulip"],
+    "Vegetables" : ["Carrot","Spinach","Broccoli"]
+
+}
+
+function category(value){
+
+    if (value.length == 0) {
+    
+        document.getElementById("choice").innerHTML = "<option></option>";
+
+    }else{
+
+        var category_options = "";
+
+        for (category_name in Category[value]){
+            category_options += "<option name='choice' value='" + Category[value][category_name] + "'>" + Category[value][category_name] + "</option>";
+        }
+
+        document.getElementById("choice").innerHTML = category_options;
+    }
+
+}
+
+</script>
+
+<select name="category" id="category" onChange="category(this.value);">
+
+    <option name="choice" value="">Select Category By Clicking Here</option>
+    <option name="category" value="Car">Car</option>
+    <option name="category" value="Food">Food</option>
+    <option name="category" value="Beer">Beer</option>
+    <option name="category" value="Fruits">Fruits</option>
+    <option name="category" value="Desserts">Desserts</option>
+    <option name="category" value="Sports">Sports</option>
+    <option name="category" value="Genres of Music">Genres of Music</option>
+    <option name="category" value="Languages">Languages</option>
+    <option name="category" value="Flowers">Flowers</option>
+    <option name="category" value="Vegetables">Vegetables</option>
+
+</select>
+
+<select name="choice" id="choice">
+
+    <option name="choice" value="">Select Category First</option>
+
+</select>
